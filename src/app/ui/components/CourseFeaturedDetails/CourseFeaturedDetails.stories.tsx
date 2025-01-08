9e55dda716d7de14d1e58871fcdd61fa01c7f758
Laura de Cos <lauradecc@gmail.com>
2025-01-08 14:53:40 +0100

    feat(86c1854u6): add course featured details story

---
+++ src/app/ui/components/CourseFeaturedDetails/CourseFeaturedDetails.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from '@storybook/react'
+import CourseFeaturedDetails from './CourseFeaturedDetails'
+
+const meta: Meta<typeof CourseFeaturedDetails> = {
+  component: CourseFeaturedDetails,
+  parameters: {
+    locale: 'es'
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof CourseFeaturedDetails>
+
+export const Default: Story = {
+  args: {
+    duration: '9 meses',
+    format: 'Presencial',
+    language: 'Español',
+    campus: 'Madrid',
+    startDate: '25 agosto'
+  }
+}
