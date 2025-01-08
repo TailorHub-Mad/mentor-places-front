7da08a09fcf8ab327b756df127cc3b9023eb9292
Laura de Cos <lauradecc@gmail.com>
2025-01-08 14:57:23 +0100

    feat(86c1854u6): remove component from homepage

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,38 +1,28 @@
 import { type FC } from 'react'
-// import client from '../lib/configs/apolloClient'
+import client from '../lib/configs/apolloClient'
 import { unstable_setRequestLocale } from 'next-intl/server'
-// import { GetCoursesDocument, GetCoursesQuery, GetCoursesQueryVariables } from '../../graphql/generated/client'
-import CourseFeaturedDetails from '@components/CourseFeaturedDetails/CourseFeaturedDetails'
-// import TestComponent from '@components/TestComponent'
+import { GetCoursesDocument, GetCoursesQuery, GetCoursesQueryVariables } from '../../graphql/generated/client'
+import TestComponent from '@components/TestComponent'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const Page: FC = async ({ params: { lng } }: any) => {
   unstable_setRequestLocale(lng)
-  // const { data } = await client.query<GetCoursesQuery, GetCoursesQueryVariables>({
-  //   query: GetCoursesDocument,
-  //   variables: {
-  //     filter: {
-  //       id: {
-  //         _eq: 'C002'
-  //       }
-  //     }
-  //   }
-  // })
-
-  const details = {
-    duration: '9 meses',
-    format: 'Presencial',
-    language: 'Español',
-    campus: 'Madrid',
-    startDate: '25 agosto'
-  }
+  const { data } = await client.query<GetCoursesQuery, GetCoursesQueryVariables>({
+    query: GetCoursesDocument,
+    variables: {
+      filter: {
+        id: {
+          _eq: 'C002'
+        }
+      }
+    }
+  })
 
   return (
     <>
       <h1>Page</h1>
-      <CourseFeaturedDetails {...details} />
-      {/* <p>{data.courses[0].id}</p>
-      <TestComponent /> */}
+      <p>{data.courses[0].id}</p>
+      <TestComponent />
     </>
   )
 }
