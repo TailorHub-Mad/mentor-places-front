60c720925870166850846a8479687ec7751f458f
Juan Núñez <juan@juannunezblasco.es>
2025-01-13 17:03:02 +0100

    fix: Limit detail length to match design

--- src/app/ui/components/CourseFeaturedDetails/CourseFeaturedDetails.tsx
+++ src/app/ui/components/CourseFeaturedDetails/CourseFeaturedDetails.tsx
@@ -3,9 +3,11 @@ import CourseDetail from './components/CourseDetail'
 import type { TAssetDetailOptions } from '@interfaces/assetDetail.type'
 
 const CourseFeaturedDetails: FC<TAssetDetailOptions> = (details) => {
+  // Limit the entries to match design
+  const limitedDetails = Object.entries(details).slice(0, 5)
   return (
     <div className="grid grid-cols-2 gap-6 lg:grid-cols-5 lg:gap-10 bg-YELLOW py-12 px-7 rounded-lg">
-      {Object.entries(details).map(([key, value]) => (
+      {limitedDetails.slice(0, 5).map(([key, value]) => (
         <CourseDetail key={`course-detail-${key}-${value}`} type={key} value={value} />
       ))}
     </div>
