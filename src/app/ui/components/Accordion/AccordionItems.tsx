0dab1ba0dff1a8e6d6a2e70275e9b3df529f84ca
Juan Núñez <juan@juannunezblasco.es>
2025-01-17 13:10:25 +0100

    refactor: Review PR

--- src/app/ui/components/Accordion/AccordionItems.tsx
+++ src/app/ui/components/Accordion/AccordionItems.tsx
@@ -17,27 +17,27 @@ const AccordionItems: FC<IAccordionItemsProps> = ({ accordionItems, firstItemOpe
   const [openItems, setOpenItems] = useState<Set<number>>(new Set([firstItemOpen ? 0 : -1]))
 
   const handleAccordion = (itemId: number) => {
-    setOpenItems((prevOpenItems) => {
-      const newOpenItems = new Set(prevOpenItems)
+    const newOpenItems = new Set(openItems)
 
-      if (newOpenItems.has(itemId)) {
-        newOpenItems.delete(itemId)
-      } else {
-        newOpenItems.add(itemId)
-      }
+    if (newOpenItems.has(itemId)) newOpenItems.delete(itemId)
+    else newOpenItems.add(itemId)
 
-      return newOpenItems
-    })
+    setOpenItems(newOpenItems)
   }
+
+  const getCollapseClass = (isOpen: boolean) =>
+    cx('h-auto overflow-hidden transition-all ease-in-out duration-500', isOpen ? 'max-h-[1000px] opacity-100' : 'max-h-0 opacity-50')
 
   return (
     <div className="flex flex-col">
       {accordionItems.map(({ title, children }, index) => (
-        <div key={index} className="relative">
+        <div key={`accordion-item-${index}-${title}`} className="relative">
           <button
             onClick={() => handleAccordion(index)}
             className="relative flex items-center w-full py-[8px] text-left transition-all ease-in border-t border-solid cursor-pointer border-BLACK/20 text-s font-s group justify-between"
-            data-collapse-target={`collapse-${index}`}>
+            data-collapse-target={`collapse-${index}`}
+            aria-expanded={openItems.has(index)}
+            aria-controls={`accordion-panel-${index}`}>
             <span className="">{title}</span>
             <ChevronArrowDown
               className={cx({
@@ -46,11 +46,11 @@ const AccordionItems: FC<IAccordionItemsProps> = ({ accordionItems, firstItemOpe
             />
           </button>
           <div
+            id={`accordion-panel-${index}`}
+            role="region"
             data-collapse={`collapse-${index}`}
-            className={cx(
-              'h-auto overflow-hidden transition-all ease-in-out duration-500',
-              openItems.has(index) ? 'max-h-[1000px] opacity-100' : 'max-h-0 opacity-50'
-            )}>
+            aria-hidden={!openItems.has(index)}
+            className={getCollapseClass(openItems.has(index))}>
             <div className="mb-4 mt-2">{children}</div>
           </div>
         </div>
