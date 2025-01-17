addf9abd32695b8b67b70c21024686eda59f8658
Laura de Cos <lauradecc@gmail.com>
2025-01-17 09:39:20 +0100

    feat(86c1c46dp): create input component

---
+++ src/app/ui/components/Input/Input.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Input from './Input'
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+  parameters: {
+    locale: 'es'
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof Input>
+
+export const Text: Story = {
+  args: {}
+}
+
+export const Password: Story = {
+  args: {
+    type: 'password'
+  }
+}
+
+export const WithError: Story = {
+  args: {
+    hasError: true
+  }
+}
---
+++ src/app/ui/components/Input/Input.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import EyeOff from '@components/icons/EyeOff'
+import EyeOn from '@components/icons/EyeOn'
+import { EColor } from '@theme/foundations/colors.foundations'
+import { cx } from '@utils/cx'
+import { InputHTMLAttributes, useState, type FC } from 'react'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  hasError?: boolean
+}
+
+const Input: FC<InputProps> = ({ hasError, type }) => {
+  const [typeState, setTypeState] = useState(type)
+
+  const isPassword = type === 'password'
+
+  return (
+    <div className="relative">
+      <input
+        type={isPassword ? typeState : type}
+        className={cx('w-full border-b pb-2 border-BLACK_10 outline-none placeholder-BLACK_60 s', { 'border-RED': hasError })}
+        placeholder="placeholder"
+      />
+      {isPassword && (
+        <div className={cx('absolute bottom-0 right-0 flex h-full items-center justify-center')}>
+          <button
+            aria-label="Change password visibility"
+            type="button"
+            onClick={() => {
+              setTypeState(typeState === 'password' ? 'text' : 'password')
+            }}>
+            {typeState === 'password' ? <EyeOff width={17} color={EColor.BLACK_40} /> : <EyeOn width={17} color={EColor.BLACK_40} />}
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Input
