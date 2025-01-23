be06adf86646c5fb0e4e74a6cacfe0388ea16cf1
Juan Núñez <juan@juannunezblasco.es>
2025-01-23 12:16:26 +0100

    feat: Meet design setting

--- src/app/ui/components/Pagination/Pagination.tsx
+++ src/app/ui/components/Pagination/Pagination.tsx
@@ -1,4 +1,7 @@
 import type { FC } from 'react'
+import ChevronArrowRight from '@components/icons/ChevronArrowRight'
+import ChevronArrowLeft from '@components/icons/ChevronArrowLeft'
+import { cx } from '@utils/cx'
 
 export interface IPaginationProps {
   page: number
@@ -8,13 +11,42 @@ export interface IPaginationProps {
 
 const Pagination: FC<IPaginationProps> = ({ totalPages, page, onChange }) => {
   const pages = Array.from({ length: totalPages }, (_, index) => index + 1)
+
+  const handlePrev = () => {
+    if (page > 1) onChange(page - 1)
+  }
+
+  const handleNext = () => {
+    if (page < totalPages) onChange(page + 1)
+  }
+
   return (
-    <div className="flex items-center gap-2">
-      {pages.map((pageNumber) => (
-        <button key={pageNumber} onClick={() => onChange(pageNumber)} style={{ fontWeight: pageNumber === page ? 'bold' : 'normal' }}>
-          {pageNumber}
-        </button>
-      ))}
+    <div className="pagination-block flex items-center justify-center gap-2">
+      <button
+        onClick={handlePrev}
+        disabled={page === 1}
+        className="pagination-block__prev flex items-center justify-center p-[20px] rounded-full bg-GRAY/60 disabled:opacity-40 disabled:cursor-not-allowed">
+        <ChevronArrowLeft className="absolute" />
+      </button>
+      <div class="pagination-block__number-loop flex items-center gap-4">
+        {pages.map((pageNumber) => (
+          <button
+            className={cx('text-s font-s', {
+              'font-bold': pageNumber === page,
+              'text-BLACK/60': pageNumber !== page
+            })}
+            key={pageNumber}
+            onClick={() => onChange(pageNumber)}>
+            {pageNumber}
+          </button>
+        ))}
+      </div>
+      <button
+        onClick={handleNext}
+        disabled={page === totalPages}
+        className="pagination-block__next flex items-center justify-center p-[20px] rounded-full bg-GRAY/60 disabled:opacity-40 disabled:cursor-not-allowed">
+        <ChevronArrowRight className="absolute" />
+      </button>
     </div>
   )
 }
