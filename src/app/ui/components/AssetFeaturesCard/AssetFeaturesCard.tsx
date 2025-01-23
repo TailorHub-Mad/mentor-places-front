19d2fb3b2b67818408602bcf4833f2290eb94ba9
Laura de Cos <lauradecc@gmail.com>
2025-01-23 16:13:20 +0100

    feat(86c1mzh44): rename array prop

--- src/app/ui/components/AssetFeaturesCard/AssetFeaturesCard.tsx
+++ src/app/ui/components/AssetFeaturesCard/AssetFeaturesCard.tsx
@@ -3,10 +3,10 @@ import type { FC } from 'react'
 interface IAssetFeaturesCardProps {
   title: string
   description: string
-  array: { label: string; number: number }[]
+  tags: { label: string; number: number }[]
 }
 
-const AssetFeaturesCard: FC<IAssetFeaturesCardProps> = ({ title, description, array }) => {
+const AssetFeaturesCard: FC<IAssetFeaturesCardProps> = ({ title, description, tags }) => {
   return (
     <div className="bg-BLUE_LIGHT rounded-lg py-8">
       <div className="px-6">
@@ -15,9 +15,9 @@ const AssetFeaturesCard: FC<IAssetFeaturesCardProps> = ({ title, description, ar
         <hr className="h-[1px] border-BLACK/10 mt-6 mb-8" />
       </div>
       <div className="flex gap-2 overflow-x-auto md:flex-wrap pl-6 md:px-6 hide-scrollbar">
-        {array.map((elm, idx) => {
+        {tags.map((elm, idx) => {
           const { label, number } = elm
-          const isLast = array.length - 1 === idx
+          const isLast = tags.length - 1 === idx
           return (
             <div key={label} className="bg-BLUE px-6 py-2 rounded-lg" style={{ marginRight: isLast ? 24 : 0 }}>
               <p className="s text-WHITE text-nowrap">{`${label} (${number})`}</p>
