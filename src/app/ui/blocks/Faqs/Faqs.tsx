9ac51c9b62e873ac1760e0f861601313c207c237
Laura de Cos <lauradecc@gmail.com>
2025-01-29 11:45:31 +0100

    feat(86c1n1z3r): create faq block component

---
+++ src/app/ui/blocks/Faqs/Faqs.tsx
@@ -0,0 +1,20 @@
+import FaqAccordion, { IFaqItem } from '@components/FaqAccordion/FaqAccordion'
+import { useTranslations } from 'next-intl'
+import type { FC } from 'react'
+
+interface IFaqsProps {
+  items: IFaqItem[]
+}
+
+const Faqs: FC<IFaqsProps> = ({ items }) => {
+  const t = useTranslations()
+
+  return (
+    <div className="flex flex-col gap-8 lg:gap-6 lg:flex-row justify-between">
+      <p className="m md:l lg:min-w-[386px]">{t('faqs')}</p>
+      <FaqAccordion items={items} />
+    </div>
+  )
+}
+
+export default Faqs
