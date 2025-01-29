e86cafca42e9cdab5b4037305dac54089eaaa066
Laura de Cos <lauradecc@gmail.com>
2025-01-29 10:45:50 +0100

    feat(86c1cef17): finish mobile view

--- src/app/ui/blocks/HeroInstitution/HeroInstitution.tsx
+++ src/app/ui/blocks/HeroInstitution/HeroInstitution.tsx
@@ -1,5 +1,6 @@
 import Stars, { type IStarsOpinionsProps } from '@components/Stars/Stars'
 import WishListButton from '@components/WishListButton/WishListButton'
+import useBreakpoint from '@hooks/useBreakpoint'
 import Image from 'next/image'
 import type { FC } from 'react'
 
@@ -11,25 +12,29 @@ interface IHeroInstitutionProps {
 }
 
 const HeroInstitution: FC<IHeroInstitutionProps> = ({ title, logo, image, opinions }) => {
+  const { isMobile } = useBreakpoint()
+
   return (
-    <div className="bg-GRAY">
-      <div className="flex justify-between">
+    <section>
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-stretch">
         <div className="flex flex-col justify-between md:min-h-[172px]">
-          <h1 className="l md:xl max-w-[577px]">{title}</h1>
+          <h1 className="l md:xl max-w-[577px] mb-8 md:mb-0">{title}</h1>
           <Stars {...opinions} />
         </div>
         <div className="flex flex-col justify-between items-end">
-          <div className="university-logo flex items-center gap-2 my-[8px]">
-            <Image src={logo} alt={''} width={44} height={44} />
-            <span className="s text-BLACK_60">{title}</span>
-          </div>
-          <WishListButton />
+          {!isMobile && (
+            <div className="university-logo flex items-center gap-2 my-[8px]">
+              <Image src={logo} alt={''} width={44} height={44} />
+              <span className="s text-BLACK_60">{title}</span>
+            </div>
+          )}
+          <WishListButton className="mt-8" />
         </div>
       </div>
       <div className="relative w-full h-[456px] mt-8 md:mt-12">
         <Image src={image} alt={title} fill objectFit="cover" className="rounded-lg" />
       </div>
-    </div>
+    </section>
   )
 }
 
--- src/app/ui/components/Stars/Stars.tsx
+++ src/app/ui/components/Stars/Stars.tsx
@@ -19,9 +19,11 @@ const StarsOpinions: FC<IStarsOpinionsProps> = ({ average, total }) => {
   const decimal = average - whole
 
   return (
-    <div className="flex gap-4 items-center">
-      <span className="s text-BLACK">{average}</span>
-      <div className="flex gap-1 items-center">{getStars(whole, decimal)}</div>
+    <div className="flex flex-col md:flex-row gap-2 md:gap-4 items-start md:items-center">
+      <div className="flex gap-4 items-center">
+        <span className="s text-BLACK">{average}</span>
+        <div className="flex gap-1 items-center">{getStars(whole, decimal)}</div>
+      </div>
       <span className="s text-BLACK/60">{`${total} ${t('opinions').toLowerCase()}`}</span>
     </div>
   )
