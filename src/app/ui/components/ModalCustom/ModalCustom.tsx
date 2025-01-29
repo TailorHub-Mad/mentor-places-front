fc9f825a711d177956e113262ccb3aa35926fd96
Juan Núñez <juan@juannunezblasco.es>
2025-01-29 17:13:11 +0100

    feat: ModalCustom initial config

---
+++ src/app/ui/components/ModalCustom/ModalCustom.tsx
@@ -0,0 +1,34 @@
+import type { FC, PropsWithChildren } from 'react'
+import { createPortal } from 'react-dom'
+import CrossIcon from '@components/icons/CrossIcon'
+
+export interface IModalCustomProps {
+  isOpen: boolean
+  handleClose: () => void
+}
+
+const ModalCustom: FC<PropsWithChildren<IModalCustomProps>> = ({ children, handleClose }) => {
+  // Create a reference to the 'modal-root' element
+  const modalRoot = document.getElementById('modal-root') as HTMLElement
+
+  // Ensure modal renders only if `modalRoot` exists
+  if (!modalRoot) {
+    console.error('No modal root element found. Please add a <div id="modal-root"></div> to your HTML.')
+    return null
+  }
+
+  return createPortal(
+    <div className="modal-custom absolute inset-0 flex items-center justify-center z-[1000]">
+      <div className="modal-custom__container bg-WHITE min-h-full md:min-h-[500px] min-w-full md:min-w-[80vw] relative z-20">
+        <button className="absolute right-[32px] top-[32px]" onClick={handleClose}>
+          <CrossIcon />
+        </button>
+        {children}
+      </div>
+      <div onClick={handleClose} className="modal-custom__overly bg-BLACK/10 absolute inset-0 z-10"></div>
+    </div>,
+    modalRoot
+  )
+}
+
+export default ModalCustom
