4b9a8ef831e68882b1d880a2b5ce4c30aa74ecd4
Juan Núñez <juan@juannunezblasco.es>
2025-01-29 18:02:01 +0100

    feat: Add banner after each 3 cards

--- src/app/ui/blocks/CoursesFeed/CoursesFeedBlock.tsx
+++ src/app/ui/blocks/CoursesFeed/CoursesFeedBlock.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react'
 import AssetCardIndex, { type IAssetCardIndexProps } from '@components/AssetCardIndex/AssetCardIndex'
+import Banner from '../Banner/Banner'
 
 interface ICoursesFeedBlockProps {
   courses: IAssetCardIndexProps[]
@@ -9,7 +10,20 @@ const CoursesFeedBlock: FC<ICoursesFeedBlockProps> = ({ courses }) => {
   return (
     <div className="courses-feed-block flex flex-col gap-[24px]">
       {courses.map((course, index) => {
-        return <AssetCardIndex key={`courses-feed-block__asset-card-${index}-${course.title}`} {...course} />
+        return (
+          <>
+            <AssetCardIndex key={`courses-feed-block__asset-card-${index}-${course.title}`} {...course} />
+            {/* Add the Banner after every 3 items */}
+            {(index + 1) % 3 === 0 && index !== courses.length - 1 && (
+              <Banner
+                key={`banner-component-${index}`}
+                action={'contact'}
+                text={'Find out which Master’s programmes match your personality!'}
+                cta={'Saber más'}
+              />
+            )}
+          </>
+        )
       })}
     </div>
   )
