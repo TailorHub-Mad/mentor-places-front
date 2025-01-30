c27fb2f4774fa42856a795e8e874644d4acad88c
Laura de Cos <lauradecc@gmail.com>
2025-01-30 18:41:46 +0100

    feat(86c1n1u6v): create CertificatesAndAwards block component

---
+++ src/app/ui/blocks/CertificatesAndAwards/CertificatesAndAwards.tsx
@@ -0,0 +1,22 @@
+import CertificationCard, { ICertificationCardProps } from '@components/CertificationCard/CertificationCard'
+import type { FC } from 'react'
+
+interface ICertificatesAndAwardsProps {
+  title: string
+  cards: ICertificationCardProps[]
+}
+
+const CertificatesAndAwards: FC<ICertificatesAndAwardsProps> = ({ title, cards }) => {
+  return (
+    <div>
+      <h3 className="text-l-mobile font-l-mobile md:text-l md:font-l">{title}</h3>
+      <div className="flex flex-col md:flex-row gap-5 mt-9 md:mt-16">
+        {cards.map((card, idx) => {
+          return <CertificationCard {...card} key={`${card.title}-${idx}`} />
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default CertificatesAndAwards
--- src/app/ui/components/CertificationCard/CertificationCard.tsx
+++ src/app/ui/components/CertificationCard/CertificationCard.tsx
@@ -9,7 +9,7 @@ export interface ICertificationCardProps {
 
 const CertificationCard: FC<ICertificationCardProps> = ({ category, title, source }) => {
   return (
-    <div className="certification-card px-[24px] py-[29px] bg-YELLOW rounded-[8px] min-h-[273px] flex flex-col justify-between">
+    <div className="certification-card px-[24px] py-[29px] bg-YELLOW rounded-[8px] min-h-[273px] flex flex-col justify-between w-full">
       <div className="certification-card__category flex items-center justify-between mb-8">
         <span className="text-m-mobile font-s font-light">{category}</span>
         <ArrowDownCorner />
