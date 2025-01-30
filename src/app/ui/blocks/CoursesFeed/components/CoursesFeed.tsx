cb7c8c1eaca3f5396d891be5e10878c336d7ae09
Juan Núñez <juan@juannunezblasco.es>
2025-01-30 16:40:23 +0100

    fix: Unique key for component on a loop

--- src/app/ui/blocks/CoursesFeed/components/CoursesFeed.tsx
+++ src/app/ui/blocks/CoursesFeed/components/CoursesFeed.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react'
+import { Fragment } from 'react'
 import AssetCardIndex, { type IAssetCardIndexProps } from '@components/AssetCardIndex/AssetCardIndex'
 import Banner, { type IBannerProps } from '../../Banner/Banner'
 
@@ -14,11 +15,11 @@ const CoursesFeed: FC<ICoursesFeedProps> = ({ courses, banner }) => {
     <div className="courses-feed-block flex flex-col gap-[24px]">
       {courses.map((course, index) => {
         return (
-          <>
-            <AssetCardIndex key={`courses-feed-block__asset-card-${index}-${course.title}`} {...course} />
+          <Fragment key={`courses-feed-block__asset-card-${index}-${course.title}`}>
+            <AssetCardIndex {...course} />
             {/* Add the Banner after second course and only once per page */}
             {index === 1 && renderBanner(index)}
-          </>
+          </Fragment>
         )
       })}
     </div>
