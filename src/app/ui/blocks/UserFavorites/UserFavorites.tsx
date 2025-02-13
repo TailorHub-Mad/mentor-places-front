dd09ea16bae8bb829ce36717aa1ea5391acccfff
Laura de Cos <lauradecc@gmail.com>
2025-02-13 15:59:54 +0100

    feat(86c1n1w46): create UserFavorites block

---
+++ src/app/ui/blocks/UserFavorites/UserFavorites.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import AssetCardIndex, { IAssetCardIndexProps } from '@components/AssetCardIndex/AssetCardIndex'
+import { SwiperNavigationPosition } from '@components/Swiper/components/SwiperNatigation'
+import CustomSwiper from '@components/Swiper/CustomSwiper'
+import useBreakpoint from '@hooks/useBreakpoint'
+import type { FC } from 'react'
+
+interface IUserFavoritesProps {
+  items: IAssetCardIndexProps[]
+  title: string
+}
+
+const UserFavorites: FC<IUserFavoritesProps> = ({ items, title }) => {
+  const { isMobile } = useBreakpoint()
+
+  return (
+    <CustomSwiper
+      position={SwiperNavigationPosition.TOP_RIGHT}
+      swiperOptions={{
+        slidesPerView: isMobile ? 1 : 3,
+        spaceBetween: 24
+      }}
+      text={title}
+      items={items.map((elm, idx) => {
+        return <AssetCardIndex key={`${idx}-user-favorites`} {...elm} isOnFavPage />
+      })}
+    />
+  )
+}
+
+export default UserFavorites
